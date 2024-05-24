364a90d9173fe1a3ae48294627f79d4170853bcb
hami <64425182+hamibey@users.noreply.github.com>
2024-05-24 22:06:18 +0300

    Initial Commit

---
+++ LoginApp2/LoginApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  LoginApp2App.swift
+//  LoginApp2
+//
+//  Created by Hami on 24.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct LoginApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
